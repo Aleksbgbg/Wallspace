a9210f142b8a8a7fd5ccafc30cd7d22739c9a334
Aleksbgbg <kozichka01@abv.bg>
2019-03-22 07:59:14 +0000

    [LoginComponent] Add form with submit handler, which prints the server's response.

--- Wallspace/WallspaceClient/src/app/components/login/login.component.ts
+++ Wallspace/WallspaceClient/src/app/components/login/login.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from "@angular/core";
+import { Component } from "@angular/core";
+
+import { AuthenticationService } from "src/app/services/authentication.service";
+import { LoginCredentials } from "src/app/models/login-credentials.model";
 
 @Component({
   selector: "app-login",
   templateUrl: "./login.component.html",
   styleUrls: ["./login.component.css"]
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
+  authToken: string;
 
-  constructor() { }
+  model = new LoginCredentials();
 
-  ngOnInit() {
+  constructor(private authenticationService: AuthenticationService) { }
+
+  onSubmit() {
+    this.authenticationService
+        .login(this.model)
+        .subscribe(response => this.authToken = JSON.stringify(response));
   }
-
 }
